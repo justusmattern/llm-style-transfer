52a2733b81cb13c12d8883612cc7bcc1f3b5a693
Justus Mattern <justus_mattern@gmx.de>
2022-05-28 21:41:32 +0200

    experimental changes

--- transfer.py
+++ transfer.py
@@ -14,23 +14,23 @@ def read_file(in_file):
     return texts
 
 
-def run(in_file, out_file, prompt_in, prompt_out, model, learning_rate)
+def run(in_file, out_file, prompt_in, prompt_out, model_name, learning_rate):
 
-    tokenizer = GPT2Tokenizer.from_pretrained(model)
+    tokenizer = GPT2Tokenizer.from_pretrained(model_name)
     in_texts = read_file(in_file)
     sim_model = SentenceTransformer('paraphrase-albert-small-v2')
 
 
     for text in in_texts:
 
-        model = GPT2LMHeadModel.from_pretrained(model)
+        model = GPT2LMHeadModel.from_pretrained(model_name)
         model.parallelize()
-        optimizer = optim.Adam(model.parameters, lr = learning_rate)
+        optimizer = optim.Adam(model.parameters(), lr = learning_rate)
 
-        for i in range(10):
+        for i in range(5):
             tokenized_text = tokenizer([prompt_in+text], return_tensors='pt').input_ids.to('cuda:0')
             tokenized_prompt = tokenizer([prompt_in], return_tensors='pt').input_ids.to('cuda:0')
-            loss = model(tokenized_text, labels=tokenized_text).loss - model(tokenized_prompt, labels=tokenized_text).loss
+            loss = model(tokenized_text, labels=tokenized_text).loss - model(tokenized_prompt, labels=tokenized_prompt).loss*len(tokenized_prompt[0])/len(tokenized_text[0])
             loss.backward()
             optimizer.step()
             print('loss', loss)
@@ -39,12 +39,12 @@ def run(in_file, out_file, prompt_in, prompt_out, model, learning_rate)
 
             sample_outputs = model.generate(
                             input_ids,
-                            do_sample=False, 
+                            do_sample=True, 
                             max_length=int(len(tokenized_text[0])*2),  
                             top_p=0.8,
                             no_repeat_ngram_size=3,
-                            num_return_sequences=0
-                        )
+                            num_return_sequences=3
+                         )
 
             generated_texts = []
             
@@ -52,9 +52,14 @@ def run(in_file, out_file, prompt_in, prompt_out, model, learning_rate)
                 text = tokenizer.decode(sample, skip_special_tokens = True)
                 generated_texts.append(text.replace(prompt_out, ''))
 
-                gen_embeddings = sim_model.encode(generated_texts)
-                orig_embedding = sim_model.encode([text])
-                sim_scores = cos_sim(gen_embeddings, orig_embedding)
+            gen_embeddings = sim_model.encode(generated_texts)
+            orig_embedding = sim_model.encode([text])
+            sim_scores = cos_sim(gen_embeddings, orig_embedding)
+            print(sim_scores)
+            print(sim_scores.shape)
+            for gen in generated_texts:
+                print()
+                print(gen.replace('\n', ' '))
 
 
 
@@ -70,4 +75,4 @@ if __name__ == '__main__':
     parser.add_argument('--learning-rate', type=float, default=1e-6)
     args = parser.parse_args()
 
-    run(in_file = args.in_file, out_file = args.out_file, prompt_in = args.prompt_in, prompt_out = args.prompt_out, model=args.model, learning_rate = args.learning_rate)
+    run(in_file = args.in_file, out_file = args.out_file, prompt_in = args.prompt_in, prompt_out = args.prompt_out, model_name=args.model, learning_rate = args.learning_rate)
